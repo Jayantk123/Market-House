708f6b0180fd603f06f29fd6cf68c4071ce1a174
Jayant Kumar <jayantk237@gmail.com>
2023-01-12 20:05:20 +0530

    create pages, routes and navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Navbar from "./Components/Navbar";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+
+        </Routes>
+        <Navbar/>
+      </Router>
+    
+    </>
+  );
 }
