0e95ff1b50ba45b887a2fe3877f76cc735755f4c
Jayant Kumar <jayantk237@gmail.com>
2023-02-25 20:14:31 +0530

    add leaflet map

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { getDoc, doc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
@@ -86,15 +87,34 @@ export default function Listing() {
         </ul>
 
         <p className="listingLocationTitle">Location</p>
-        {/* map */}
+
+        <div className="leafletContainer">
+          <MapContainer
+            style={{ height: "100%", width: "100%" }}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+            <Popup>{listing.location}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
 
         {auth.currentUser?.uid !== listing.userRef && (
-          <Link to={`/contact/${listing.userRef}?listingName=${listing.name}`} className='primaryButton'>
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}`}
+            className="primaryButton"
+          >
             Contact Landlord
           </Link>
         )}
       </div>
     </main>
-   
   );
 }
